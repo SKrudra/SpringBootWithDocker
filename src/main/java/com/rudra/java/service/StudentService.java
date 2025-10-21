2b86feef33ef4976138a38e0d22e88559b17176c
Sandeep <skrudra12@gmail.com>
2025-10-21 17:26:56 -0500

    1. Add /students endpoint

---
+++ src/main/java/com/rudra/java/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.rudra.java.service;
+
+import com.rudra.java.entity.Student;
+import com.rudra.java.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> findAll(){return studentRepository.findAll();}
+}
